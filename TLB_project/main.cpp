5f33d9fb08485e1d90ac4e4b0d799fccc74a3119
jdevries24 <77797747+jdevries24@users.noreply.github.com>
2022-03-16 14:52:32 -0400

    Delete TLB_project directory

--- TLB_project/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "TLB_demo.cpp"
-using namespace std;
-
-int main(){
-   TLB_demo demo = TLB_demo("addresses.txt","BACKING_STORE.bin","my_output.txt");
-   demo.run();
-}
